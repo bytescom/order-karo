b0efc69be08500154046c8032ce5f3f7c50a172b
bytescom <pankajk07414@gmail.com>
2026-05-19 14:42:31 +0530

    implement complete authentication with JWT and cookies

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,47 @@
-import express from 'express'
-import { PORT } from './src/config/index.js'
-import { connectDB } from './src/config/db.config.js'
-import cors from 'cors'
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import { FRONTEND_URL } from "./src/config/index.js";
+import { PORT } from "./src/config/index.js";
+import { connectDB } from "./src/config/db.config.js";
+import authRoutes from "./src/routes/auth.route.js";
 
 connectDB();
 
-const app = express()
+const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
-app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.use(cookieParser());
 
-app.get('/api/test',(req, res)=>{
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/auth", authRoutes);
+
+app.get('/api/auth/test',(req, res)=>{
    res.json({
     success : true,
-    message : "Api working successfully"
+    message : "Auth route is working successfully"
    })
 })
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:3000')
 })
+
+export default app;
--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -56,12 +56,10 @@ const userSchema = new mongoose.Schema(
 
 
 // ! password encryption
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return;
-  }
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
   this.password = await bcrypt.hash(this.password, 10);
-  // next();
 });
 
 userSchema.methods.comparePassword = async function (password) {
