4b9ee4925e173caf8409f8ee2abf0bc84a89a556
bytescom <pankajk07414@gmail.com>
2026-05-19 13:51:34 +0530

    src creation with directory structure, db models creation for projects

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -2,4 +2,9 @@ import dotenv from "dotenv";
 dotenv.config({ quiet: true });
 
 export const PORT = process.env.PORT;
+
 export const MONGODB_URL = process.env.MONGODB_URL;
+
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+export const FRONTEND_URL = process.env.FRONTEND_URL;
